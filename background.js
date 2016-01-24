efecce44031738813e55fa4c19d2d01160c7084a
zhangyunhao <zhangyunhao@diditaxi.com.cn>
2016-01-24 17:04:38 +0800

    first

---
+++ background.js
@@ -0,0 +1,22 @@
+function show() {
+  var opt = {
+        type: "list",
+        title: "桌面提醒",
+        message: "msg",
+        iconUrl: "icon.png",
+        items: [{ title: "订餐啦", message: "现在时间三点半"}]
+      }
+  chrome.notifications.create('',opt,function(id){})
+}
+
+function checkForTime() {
+    var myDate = new Date();
+    if(myDate.getDay != 0 && myDate.getDay != 6 && myDate.getHours() == 15 && myDate.getMinutes() == 30){
+        show();
+        var createProperties = {
+        	url: "http://meican.com/corps/589067"
+        };
+        chrome.tabs.create(createProperties, function(tab){});
+    }
+}
+setInterval(checkForTime, 31000);
